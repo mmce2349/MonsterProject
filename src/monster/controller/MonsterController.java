8e9ca77d8751ba46663901d45395292116e51a9f
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-09-15 09:38:54 -0600

    Added Scanner

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,8 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster; 
+import java.util.Scanner;
+
 
 public class MonsterController {
 	
@@ -9,5 +11,25 @@ public class MonsterController {
 			MarshmallowMonster sample = new MarshmallowMonster();
 			System.out.println(sample);
 			MarshmallowMonster realMonster = new MarshmallowMonster("Bob", 4, 2, 3.5, true);
+			
+			System.out.println(realMonster);
+			System.out.println("Mitch is hungry, so he is going to eat a  tentacle");
+			realMonster.setTentacleAmount(1);
+			System.out.println(realMonster);
+			
+			interactWithTheMonster(realMonster);
+			
+	}
+	
+	private void interactWithTheMonster(MarshmallowMonster currentMonster)
+	{
+		System.out.println(currentMonster.getName() + " wants to know what to eat next");
+		System.out.println(currentMonster.getName() + " suggests arms, they have "+ currentMonster.getArmCount());
+		System.out.println("How many do you want to eat?");
+		Scanner myScanner = new Scanner(System.in);
+		int consumed = myScanner.nextInt();
+		currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+		System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
+		
 	}
 }
